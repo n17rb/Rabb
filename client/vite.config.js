94cea0b69dc7dc452e58423357de5951f810abb0
n17rb <140302157+n17rb@users.noreply.github.com>
2026-09-12 20:03:42 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": "http://localhost:4000",
-      "/uploads": "http://localhost:4000",
-    },
+  build: {
+    outDir: "dist",
   },
 });
